faeb7f95c42f0a30f68970b578ea68dfbceddb56
hannahdwyer <hannahdwyer@10.232.169.207>
2024-10-02 13:42:23 -0500

    code for flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,18 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color green1 = new Color(22,155,98);
+	
+		StdDraw.setPenRadius(0.05);
+        StdDraw.setPenColor(green1);
+        StdDraw.filledRectangle(.2, .3, 0.1, 0.2);
+        StdDraw.setPenColor(StdDraw.WHITE);
+        StdDraw.filledRectangle(.4, .3, 0.1, 0.2);
+        Color orange = new Color(255,136,62);
+        StdDraw.setPenColor(orange);
+        StdDraw.filledRectangle(.6, .3, 0.1, 0.2);
+         
+        
 		
 	}
 }
